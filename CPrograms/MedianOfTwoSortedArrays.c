80d22d738b9bb6fed5e4d4d37e6ca648d7508227
Tharun R <rajtharun08@gmail.com>
2025-08-04 17:04:04 +0530

    added Median of two sorted arrays

---
+++ CPrograms/MedianOfTwoSortedArrays.c
@@ -0,0 +1,31 @@
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    
+   int totalSize = nums1Size + nums2Size;
+    int merged[totalSize];
+
+    int i = 0, j = 0, k = 0;
+    while (i < nums1Size && j < nums2Size) {
+        if (nums1[i] <= nums2[j]) {
+            merged[k++] = nums1[i++];
+        } else {
+            merged[k++] = nums2[j++];
+        }
+    }
+
+    while (i < nums1Size) {
+        merged[k++] = nums1[i++];
+    }
+    while (j < nums2Size) {
+        merged[k++] = nums2[j++];
+    }
+
+    if (totalSize % 2 == 0) {
+        // If the total number of elements is even
+        int mid1 = merged[totalSize / 2 - 1];
+        int mid2 = merged[totalSize / 2];
+        return (double)(mid1 + mid2) / 2;
+    } else {
+        // If the total number of elements is odd
+        return merged[totalSize / 2];
+    }
+}
