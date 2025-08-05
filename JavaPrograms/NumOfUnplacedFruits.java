2d08101da8bee045f0b1dbf7bea57c769fc04d63
Tharun R <rajtharun08@gmail.com>
2025-08-05 17:47:50 +0530

    added Num of unplaced fruits problem

---
+++ JavaPrograms/NumOfUnplacedFruits.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] basket) 
+    {
+        boolean[] used =new boolean[basket.length];
+        int count=0;
+        for(int fruit:fruits)
+        {
+            boolean placed=false;
+            for(int i=0;i<basket.length;i++)
+            {
+                if(!used[i] && basket[i]>=fruit)
+                {
+                    used[i]=true;
+                    placed=true;
+                    break;
+                }
+            }
+            if(!placed)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+} 
