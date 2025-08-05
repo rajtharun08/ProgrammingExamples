ffb21360c57dde6a9fe0e5138083b9a78e4211ed
Tharun R <rajtharun08@gmail.com>
2025-08-05 17:51:16 +0530

    added Spiral Matrix problem

---
+++ JavaPrograms/SprialMatrix.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public List<Integer> spiralOrder(int[][] arr) 
+    {
+        List<Integer> res=new ArrayList<>();
+        int rows=arr.length;
+        int cols=arr[0].length;
+        int top=0,bottom=rows-1,left=0,right=cols-1;
+        while(top<=bottom && left<=right)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                res.add(arr[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++)
+            {
+                res.add(arr[i][right]);
+            }
+            right--;
+            if(top<=bottom)
+            {
+                for(int i=right;i>=left;i--)
+                {
+                    res.add(arr[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right)
+            {   
+                for(int i=bottom;i>=top;i--)
+                {
+                    res.add(arr[i][left]);
+                }
+                left++;
+            }    
+        }
+        return res;
+    }
+}
