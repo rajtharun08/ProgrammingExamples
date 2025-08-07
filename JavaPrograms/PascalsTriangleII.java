cd95edb5c8e2927f5110e38c6a4c1c5321b861c2
Tharun R <rajtharun08@gmail.com>
2025-08-07 18:19:55 +0530

    added Pascals's Triangle II program

---
+++ JavaPrograms/PascalsTriangleII.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int ncr(int n, int r) {
+        long val = 1;
+        for (int i = 0; i < r; i++) {
+            val *= (n - i);
+            val /= (i + 1);
+        }
+        return (int) val;
+    }
+
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            res.add(ncr(rowIndex, i));
+        }
+        return res;
+    }
+}
