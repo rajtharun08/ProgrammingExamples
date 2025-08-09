fe5810aad5547dc1a5652539dfa2ea9fa652e756
Tharun R <rajtharun08@gmail.com>
2025-08-09 12:22:50 +0530

    added container with most water problem

---
+++ JavaPrograms/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxArea(int[] height) 
+    {
+        int low=0;
+        int high=height.length - 1;
+        int maxArea=0;
+        while(low<high)
+        {
+            int h=Math.min(height[low],height[high]);
+            int w=high-low;
+            maxArea=Math.max(maxArea,h*w);
+            if(height[low]<height[high])
+            {
+                low++;
+            }
+            else
+            {
+                high--;
+            }
+        }
+        return maxArea;
+    }
+}
---
+++ JavaPrograms/KthSmallestDivisor.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(int[] arr,int t,int n)
+    {
+        int sum=0;
+        for(int i:arr)
+        {
+            sum+=Math.ceil((double)i/n);
+        }
+        return sum<=t;
+    }
+    public int smallestDivisor(int[] arr, int t) {
+        int low=0;
+        int high=0;
+        for(int i:arr)
+        {
+            high=Math.max(high,i);
+        }
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(isValid(arr,t,mid))
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return low;
+    }
