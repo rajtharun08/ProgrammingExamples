69307a75a63369d2dabbf098d5b3d8480f1205c5
Tharun R <rajtharun08@gmail.com>
2025-08-10 08:58:07 +0530

    added Faulty keyboard program

---
+++ JavaPrograms/FaultyKeyboard.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray())
+        {
+            if(c!='i')
+            {
+                sb.append(c);
+            }
+            else
+            {
+                sb.reverse();
+            }
+        }
+        return sb.toString();
+    }
+}
