85a7cc8133e7d979a7a213607cac6551a5bd2813
Tharun R <rajtharun08@gmail.com>
2025-08-14 21:39:10 +0530

    added search insert position problem

---
+++ JavaPrograms/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+public class SearchInsertPosition {
+	    public int searchInsert(int[] arr, int target) 
+	    {
+	        int low=0,high=arr.length-1;
+	        while(low<=high)
+	        {
+	            int mid=low+(high-low)/2;
+	            if(arr[mid]==target)
+	            {
+	                return mid;
+	            }
+	            else if(arr[mid]>target)
+	            {
+	                high=mid-1;
+	            }
+	            else
+	            {
+	                low=mid+1;
+	            }
+	        }
+	        return low;
+	    }
+}
