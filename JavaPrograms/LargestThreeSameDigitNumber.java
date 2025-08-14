3c8eb1a303ddbc0843a6389b9f312100c0c14fbf
Tharun R <rajtharun08@gmail.com>
2025-08-14 15:37:01 +0530

    Added Largest Three digit number in String program

---
+++ JavaPrograms/LargestThreeSameDigitNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String largestGoodInteger(String nums) 
+    {
+        String max="";
+        int n=nums.length();
+        for(int i=0;i<n-2;i++)
+        {
+            char c=nums.charAt(i);
+            if(c==nums.charAt(i+1) && c==nums.charAt(i+2))
+            {
+                String threeDigit=nums.substring(i,i+3);
+                if(max.isEmpty() || threeDigit.compareTo(max)>0)
+                {
+                    max=threeDigit;
+                }
+            }
+        }
+        return max;
+    }
+}
