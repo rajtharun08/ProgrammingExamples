5a669cb92bdbef3f652ca46788e57772287cac9b
Tharun R <rajtharun08@gmail.com>
2025-08-23 15:23:39 +0530

    added Hamming Distance program

---
+++ JavaPrograms/HammingDistance.java
@@ -0,0 +1,19 @@
+public class HammingDistance {
+    class Solution {
+    public int hammingWeight(int n) 
+    {
+        int count=0;
+        while(n!=0)
+        {
+            count++;
+            n=n&(n-1);
+        }
+        return count;
+    }
+    public int hammingDistance(int x, int y) 
+    {
+        int res=hammingWeight(x^y);
+        return res;
+    }
+}
+}
