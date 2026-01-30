833f046ede7e27fdfe556694366080887fbf0e5c
Tharun R <rajtharun08@gmail.com>
2026-01-30 09:30:29 +0530

    added override annotation for overridden functions

--- JavaPrograms/payroll_system/ContractEmployee.java
+++ JavaPrograms/payroll_system/ContractEmployee.java
@@ -1,6 +1,7 @@
 package payroll_system;
 
 public class ContractEmployee extends Employee {
+	@Override
 	public void calculateSalary() {
 		System.out.println("salary : "+getBasicSalary());
 	}
--- JavaPrograms/payroll_system/PermanentEmployee.java
+++ JavaPrograms/payroll_system/PermanentEmployee.java
@@ -3,6 +3,8 @@ package payroll_system;
 public class PermanentEmployee extends Employee {
 	int hra=5000;
 	int da=1200;
+	
+	@Override
 	public void calculateSalary() {
 		int sal=getBasicSalary()+hra+da;
 		setBasicSalary(sal);
